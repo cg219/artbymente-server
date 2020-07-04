4f9aacd6306abaea72ec913493666a4d4ad14882
Clemente <dev@imkreative.com>
2020-07-04 01:39:03 -0400

    Add Tests and Deploy Instructions

--- tests/test.js
+++ tests/test.js
@@ -9,13 +9,28 @@ afterAll(() => {
     console.log('Testing Completed');
 });
 
-describe('Test All Posts', () => {
+describe('Test All Artworks', () => {
 
     beforeAll(async () => {
         response = await request(app).get('/api/artworks');
     })
 
     test('check if posts exists', () => {
-        expect(response.body).toHaveProperty('status');
+        expect(response.body).toHaveProperty('data');
+    })
+})
+
+describe('Test Single Artworke', () => {
+
+    beforeAll(async () => {
+        response = await request(app).get('/api/artworks');
+
+        let slug = response.body.data[0].slug;
+
+        response = await request(app).get(`/api/artworks/${slug}`);
+    })
+
+    test('check if data exists', () => {
+        expect(response.body).toHaveProperty('data');
     })
 })
